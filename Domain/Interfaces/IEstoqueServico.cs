1429afb7f8ca65ec19ce5e5d51dd6de3ca7511c7
Marcelo Rocha Gomes <marcelokm15@gmail.com>
2024-10-18 03:42:26 -0300

    CRUD Estoque Finalizado

---
+++ Domain/Interfaces/IEstoqueServico.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Entidades;
+
+namespace Domain.Interfaces
+{
+    public interface IEstoqueServico
+    {
+        void InserirEstoque(Estoque estoque);
+        List<Estoque> ObterProdutoPorIdLoja(int idLoja);
+        List<Estoque> ObterProdutoPorIdProduto(int idProduto);
+        Estoque ObterEstoquePorIdLojaEIdProduto(int idProduto, int idLoja);
+        void AtualizarEstoquePorId(Estoque estoque);
+        Estoque ObterEstoquePorId(int idProduto);
+        void DeletarEstoquePorId(int idProduto);
+    }
+}
