05ff1841584ad78e2b6e60447109dd07480d7d0d
Eliton Alves <elitonmeunome_2014@hotmail.com>
2024-11-03 03:06:19 -0300

    ATTT

--- WorkerXepaFood/Worker.cs
+++ WorkerXepaFood/Worker.cs
@@ -1,24 +1,71 @@
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+using System.Threading.Channels;
+
 namespace WorkerXepaFood
 {
     public class Worker : BackgroundService
     {
         private readonly ILogger<Worker> _logger;
+        private IConnection _connection;
+        private IModel _channel;
 
         public Worker(ILogger<Worker> logger)
         {
             _logger = logger;
+            InitializeRabbitMQ();
         }
 
-        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        private void InitializeRabbitMQ()
         {
-            while (!stoppingToken.IsCancellationRequested)
+            
+       
+
+        }
+
+        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            var factory = new ConnectionFactory { HostName = "localhost" };
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+
+            channel.QueueDeclare(
+                queue: "QUEUE.PRODUTO.SQL",
+                durable: true,
+                exclusive: false,
+                autoDelete: false,
+                arguments: null
+            );
+
+            var consumer = new EventingBasicConsumer(channel);
+
+            consumer.Received += async (model, ea) =>
             {
-                if (_logger.IsEnabled(LogLevel.Information))
+                try
                 {
-                    _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+                    byte[] body = ea.Body.ToArray();
+                    var message = Encoding.UTF8.GetString(body);
+                    Console.WriteLine($" [x] Process Queue {ea.RoutingKey} body {message}");
+
+
+                    int dots = message.Split('.').Length - 1;
+                    Thread.Sleep(dots * 1000);
+
+                    Console.WriteLine(" [x] Done");
+
+                    channel.BasicAck(deliveryTag: ea.DeliveryTag, multiple: false);
                 }
-                await Task.Delay(1000, stoppingToken);
-            }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Error Worker {ex.Message}");
+                    channel.BasicNack(deliveryTag: ea.DeliveryTag, multiple: false, requeue: true);
+                }
+            };
+
+            channel.BasicConsume(queue: "QUEUE.PRODUTO.SQL", autoAck: true, consumer: consumer);
+
+            return Task.CompletedTask;
         }
     }
 }
